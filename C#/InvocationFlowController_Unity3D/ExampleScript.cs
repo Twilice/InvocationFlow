32066b6d250120d0e13cee98b127424ab0756b72
Tobias Löf Melker <molkiar@gmail.com>
2020-09-19 21:24:37 +0200

    added example for destroyed gameobjects

--- C#/InvocationFlowController_Unity3D/ExampleScript.cs
+++ C#/InvocationFlowController_Unity3D/ExampleScript.cs
@@ -14,6 +14,9 @@ public class ExampleScript : MonoBehaviour
         // Other Invokes exist as well
         this.InvokeWhile(AboveMagicTreshold, () => print("We are above the treshold!"));
         this.TimeLerpValueThen(2f, 0f, 100f, (value) => print($"Loading {value}%"), () => print("Loading completed in 2 seconds!"));
+        
+        // If you need to do an invocation and the gameobject might be destroyed, it can be bound to a hidden Flow Gameobject.
+        Flow.InvokeDelayed(5f, () => print("5 seconds passed."));
     }
 
     private void PrintFunc()
